8a6e82fea7889f25f2b1aa9b631a63d8efa2d12e
0RVB3N0 <97003214+0RVB3N0@users.noreply.github.com>
2024-04-16 17:08:06 +0200

    Añadido soporte para FullCalendar en el componente Dashboard y creado otro formulario para crear clubes

--- src/app/Modules/dashboard/dashboard.component.ts
+++ src/app/Modules/dashboard/dashboard.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import { CalendarOptions } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
+  calendarOptions: CalendarOptions = {
+    initialView: 'dayGridMonth',
+    plugins: [dayGridPlugin],
+    events: [
+      { title: 'event 1', date: '2024-04-22' }
+    ],
+    headerToolbar: {
+      left: 'prev,next',
+      center: 'title',
+      right: 'dayGridMonth,dayGridWeek,dayGridDay'
+    },
+    windowResize: function (arg) {
+      const windowWidth = window.innerWidth;
+      if (windowWidth < 1000) {
+        arg.view.calendar.changeView('dayGridDay');
 
+      } else if (windowWidth < 1445) {
+        arg.view.calendar.changeView('dayGridWeek');
+      } else {
+        arg.view.calendar.changeView('dayGridMonth');
+      }
+    }
+  };
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { FullCalendarModule } from '@fullcalendar/angular';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -7,7 +8,6 @@ import { LoginComponent } from './Modules/login/login.component';
 import { DashboardComponent } from './Modules/dashboard/dashboard.component';
 import { RegisterComponent } from './Modules/register/register.component';
 import { PasswordForgetComponent } from './Modules/password-forget/password-forget.component';
-
 
 @NgModule({
   declarations: [
@@ -19,7 +19,8 @@ import { PasswordForgetComponent } from './Modules/password-forget/password-forg
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FullCalendarModule
   ],
   providers: [
     provideClientHydration()
