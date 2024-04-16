0efcebeaff98560f56831454f460ef17302811a9
0RVB3N0 <97003214+0RVB3N0@users.noreply.github.com>
2024-04-16 17:37:41 +0200

    Traducido Full Calendar al Español

--- src/app/Modules/dashboard/dashboard.component.ts
+++ src/app/Modules/dashboard/dashboard.component.ts
@@ -8,22 +8,25 @@ import dayGridPlugin from '@fullcalendar/daygrid';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
+
   calendarOptions: CalendarOptions = {
     initialView: 'dayGridMonth',
     plugins: [dayGridPlugin],
     events: [
-      { title: 'event 1', date: '2024-04-22' }
+      { title: 'Cumple Ruben', date: '2024-04-14' }
     ],
     headerToolbar: {
-      left: 'prev,next',
+      left: 'prev,next today',
       center: 'title',
       right: 'dayGridMonth,dayGridWeek,dayGridDay'
     },
+    locale: 'es',
+    buttonText: { today: "Hoy", dayGridMonth: 'Mes', dayGridWeek: 'Semana', dayGridDay: "Dia" }, // Establece el texto del botón "Hoy" en español
+
     windowResize: function (arg) {
       const windowWidth = window.innerWidth;
       if (windowWidth < 1000) {
         arg.view.calendar.changeView('dayGridDay');
-
       } else if (windowWidth < 1445) {
         arg.view.calendar.changeView('dayGridWeek');
       } else {
