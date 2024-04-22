60d57011358239065287bfdc9679c2b29213601b
Alejandra Garcia Jacobo <alejgarciaj@gmail.com>
2024-04-22 16:12:26 +0200

    Login done

---
+++ src/app/Core/Services/usuario/auth-usuario.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../../../environments/environment';
+import {HttpClient} from "@angular/common/http";
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthUsuarioService {
+
+  constructor(private http: HttpClient) { }
+
+
+  iniciarSesion(email: string, contrasena: string):Observable<any>{
+
+    const body = { correo: email, contrasena: contrasena };
+    
+    return this.http.post<any>(environment.url+"/api/iniciarSesion", body);
+    
+  }
+}
--- src/app/Modules/login/login.component.ts
+++ src/app/Modules/login/login.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { AuthUsuarioService } from '../../Core/Services/usuario/auth-usuario.service';
 
 @Component({
   selector: 'app-login',
@@ -7,4 +8,103 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  email:string = "";
+  contrasena:string = "";
+  contrasenaMal:boolean = false;
+  emailMal:boolean = false;
+  mensajeErrorEmail:string = "";
+  mensajeErrorContrasena:string = "";
+  formatoCorrecto:boolean = true;
+
+  comprobandoLogin:boolean = false;
+  constructor(private authUsuario: AuthUsuarioService){}
+
+  iniciarSesion(){
+    
+    this.resetearEstilos();
+    this.comprobarFormatoLogin();
+    if(this.formatoCorrecto){
+      this.comprobandoLogin = true;
+      this.authUsuario.iniciarSesion(this.email, this.contrasena).subscribe(
+        (response) => {
+         
+          if(response['ok'] === 'ok'){
+            alert("login correcto");
+          }
+          else{
+            if(response['correo'] == 'correoIncorrecto'){
+              this.emailIncorrecto();
+              this.formatoCorrecto = false;
+              this.mensajeErrorEmail = 'Correo incorrecto';
+            }
+            if(response['contrasena'] == 'contrasenaIncorrecta'){
+              this.contrasenaIncorrecta();
+              this.formatoCorrecto = false;
+              this.mensajeErrorContrasena = 'Contraseña incorrecta';
+            }
+          }
+
+          this.comprobandoLogin = false;
+        }
+        
+      );
+    }
+      
+  }
+
+  
+
+  contrasenaIncorrecta() {
+
+    let inputContrasena = document.getElementById("password");
+    if(inputContrasena != null) inputContrasena.style.border = "1px solid red";
+
+    this.contrasenaMal = true;
+    
+  }
+
+  emailIncorrecto(){
+
+    let inputEmail = document.getElementById("email");
+    if(inputEmail!= null) inputEmail.style.border = "1px solid red";
+    this.emailMal = true;
+
+  }
+
+  resetearEstilos(){
+    
+    let inputContrasena = document.getElementById("password");
+    if(inputContrasena != null) inputContrasena.style.border = "1px solid grey";
+    let inputEmail = document.getElementById("email");
+    if(inputEmail!= null) inputEmail.style.border = "1px solid grey";
+    this.emailMal = false;
+    this.contrasenaMal = false;
+    this.formatoCorrecto = true;
+  }
+
+  comprobarFormatoLogin(){
+   
+    if(this.contrasena == ""){
+      this.contrasenaIncorrecta();
+      this.formatoCorrecto = false;
+      this.mensajeErrorContrasena = "Introduzca una contraseña";
+    }
+    if(this.contrasena.length < 7){
+      this.contrasenaIncorrecta();
+      this.formatoCorrecto = false;
+      this.mensajeErrorContrasena = "Introduzca una contraseña válida";
+    }
+    if(this.email == ""){
+      this.emailIncorrecto();
+      this.formatoCorrecto = false;
+      this.mensajeErrorEmail= "Introduzca un email";
+    }
+    if (this.email.indexOf('@') == -1) {
+      this.emailIncorrecto();
+      this.formatoCorrecto = false;
+      this.mensajeErrorEmail= "Introduzca un email correcto";
+    }
+
+    
+  }
 }
