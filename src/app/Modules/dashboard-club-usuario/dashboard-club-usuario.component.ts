211ebac78a6a47109ae2532fa2f2d68302c80009
0RVB3N0 <97003214+0RVB3N0@users.noreply.github.com>
2024-05-06 23:35:40 +0200

    Corregir estilos en el componente dashboard-club-usuario

--- src/app/Modules/dashboard-club-usuario/dashboard-club-usuario.component.ts
+++ src/app/Modules/dashboard-club-usuario/dashboard-club-usuario.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router'; // Import ActivatedRoute
+import { obtenerSessionUsuario } from '../../shared/guardarSessionUsuario/guardarSessionUsuario';
+import { SessionUsuario } from '../../Core/Models/session.model';
 
 @Component({
   selector: 'app-dashboard-club-usuario',
@@ -6,5 +9,23 @@ import { Component } from '@angular/core';
   styleUrl: './dashboard-club-usuario.component.css'
 })
 export class DashboardClubUsuarioComponent {
+  usuarioLogeado: SessionUsuario;
+  id_club: number | null = null;
+  nombreClub: string = "";
+  constructor(private route: ActivatedRoute) {
+    this.usuarioLogeado = obtenerSessionUsuario();
+  }
+
+  ngOnInit(): void {
+    // Extract the clubId from the query parameters
+    this.route.queryParams.subscribe(params => {
+      const clubId = params['clubId'];
+      const clubName = params['nombreClub'];
+      if (clubId) {
+        this.id_club = Number(clubId); // Convert to a number and assign
+        this.nombreClub = clubName;
+      }
+    });
+  }
 
 }
