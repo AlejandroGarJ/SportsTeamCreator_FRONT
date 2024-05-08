10464497ca55c17d151c8c75167bb7e7fdc06c56
0RVB3N0 <97003214+0RVB3N0@users.noreply.github.com>
2024-05-08 15:34:59 +0200

    Corregir importaciones y agregar ToastrService en perfil.component.ts

--- src/app/Modules/Usuario/perfil/perfil.component.ts
+++ src/app/Modules/Usuario/perfil/perfil.component.ts
@@ -1,12 +1,13 @@
 
 import { Component } from '@angular/core';
-import {Router} from "@angular/router";
+import { Router } from "@angular/router";
 import { SessionUsuario } from '../../../Core/Models/session.model';
 import { obtenerSessionUsuario } from '../../../shared/guardarSessionUsuario/guardarSessionUsuario';
 import { InfoUsuarioService } from '../../../Core/Services/usuario/info-usuario.service';
 import { Usuario, UsuarioInit } from '../../../Core/Models/usuario.model';
 import { environment } from '../../../../environments/environment';
 import { HttpClient } from '@angular/common/http';
+import { ToastrService } from 'ngx-toastr';
 @Component({
   selector: 'app-perfil',
   templateUrl: './perfil.component.html',
@@ -18,24 +19,24 @@ export class PerfilComponent {
   usuario: Usuario = UsuarioInit;
   modificando: boolean = false;
 
-  constructor(private router:Router, private infoUsuario: InfoUsuarioService, private http: HttpClient){
+  constructor(private router: Router, private infoUsuario: InfoUsuarioService, private http: HttpClient, private toastr: ToastrService) {
     this.usuarioLogeado = obtenerSessionUsuario();
 
     this.obtenerInfoUsuario();
   }
 
-  ngOnInit(){
-   
+  ngOnInit() {
+
   }
-  irCambioContrasena(){
+  irCambioContrasena() {
     this.router.navigate(['correo-cambio-contrasena']);
   }
-  irAtras(){
+  irAtras() {
     this.router.navigate(['dashboard']);
   }
 
 
-  obtenerInfoUsuario(){
+  obtenerInfoUsuario() {
     this.infoUsuario.info(this.usuarioLogeado.token_session, this.usuarioLogeado.dni).subscribe(
       (response) => {
         this.usuario.nombre = response.nombre;
@@ -44,21 +45,29 @@ export class PerfilComponent {
       }
     );
   }
- 
-  guardar(){
+
+  guardar() {
     this.modificando = true;
-    if(this.usuario.nombre != "" && this.usuario.apellidos != "" && this.usuario.correo != ""){
-      if(this.usuario.correo.indexOf('@') != -1){
-        this.http.post<any>(environment.url+"/api/modificarUsuario", { token_session: this.usuarioLogeado.token_session, nombre: this.usuario.nombre, apellidos: this.usuario.apellidos, correo: this.usuario.correo}).subscribe(
-        (response) => {
-          console.log(response);
-          this.modificando = false;
-        },
-        (error) => {
-          console.log(error);
-          this.modificando = false;
-        }
-        
+    if (this.usuario.nombre != "" && this.usuario.apellidos != "" && this.usuario.correo != "") {
+      if (this.usuario.correo.indexOf('@') != -1) {
+        this.http.post<any>(environment.url + "/api/modificarUsuario", { token_session: this.usuarioLogeado.token_session, nombre: this.usuario.nombre, apellidos: this.usuario.apellidos, correo: this.usuario.correo, dni: this.usuarioLogeado.dni }).subscribe(
+          (response) => {
+            console.log(response);
+            if (response === "ok") {
+              this.toastr.success("Usuario modificado correctamente");
+              this.usuarioLogeado.nombre = this.usuario.nombre;
+              localStorage.setItem('sessionUsuario', JSON.stringify(this.usuarioLogeado));
+            } else {
+              this.toastr.error("Error al modificar el usuario");
+              this.obtenerInfoUsuario();
+            }
+            this.modificando = false;
+          },
+          (error) => {
+            console.log(error);
+            this.modificando = false;
+          }
+
         );
 
       }
